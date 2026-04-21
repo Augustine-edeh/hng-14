26c143dfb3f09e07df590f4c033ff9d2eb2476bb
Augustine Edeh <info.augustinesedeh@gmail.com>
2026-04-21 19:13:55 +0100

    chore: modify filterbar component

--- stage-2/invoice-management-app/src/components/FilterBar.tsx
+++ stage-2/invoice-management-app/src/components/FilterBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import {
   Popover,
   PopoverContent,
@@ -16,32 +17,47 @@ interface Props {
 export function FilterBar({ activeFilter, onFilterChange }: Props) {
   const filters = ["draft", "pending", "paid"];
 
+  const [open, setOpen] = useState(false);
+
   return (
-    <Popover>
+    <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
-        <button className="flex items-center gap-2 text-sm font-medium text-invoice-text-primary dark:text-invoice-text-light">
+        <button className="flex items-center gap-2 text-sm font-medium dark:text-invoice-text-light">
           <span className="hidden sm:inline">Filter by status</span>
           <span className="sm:hidden">Filter</span>
-          <ChevronDown size={16} className="text-invoice-primary" />
+
+          {/* 👇 Chevron */}
+          <ChevronDown
+            size={16}
+            className={`
+              text-invoice-primary
+              transition-transform duration-200
+              ${open ? "rotate-180" : "rotate-0"}
+            `}
+          />
         </button>
       </PopoverTrigger>
 
-      <PopoverContent
-        align="end"
-        className="w-48 p-4 rounded-xl shadow-lg bg-white dark:bg-[#1E2139]"
-      >
+      <PopoverContent className="w-48 p-4 mt-5 rounded-[8px] shadow-lg bg-white dark:bg-[#1E2139]">
         <div className="flex flex-col gap-3">
           {filters.map((filter) => (
             <label
               key={filter}
-              className="flex items-center gap-3 cursor-pointer"
+              className="
+                flex items-center gap-3 cursor-pointer
+                hover:bg-gray-100 dark:hover:bg-[#252945]
+                px-2 py-1 rounded-md
+              "
             >
               <Checkbox
                 checked={activeFilter === filter}
                 onCheckedChange={() => onFilterChange(filter)}
+                className="data-checked:bg-invoice-primary bg-invoice-text-light focus-visible:ring-1 rounded-xs focus-visible:ring-invoice-primary focus-visible:ring-offset-1 focus-visible:ring-offset-white dark:focus-visible:ring-offset-[#1E2139] dark:bg-gray-700 dark:data-checked:bg-invoice-primary"
               />
 
-              <span className="capitalize text-sm font-medium">{filter}</span>
+              <span className="capitalize text-sm font-bold text-black">
+                {filter}
+              </span>
             </label>
           ))}
         </div>
